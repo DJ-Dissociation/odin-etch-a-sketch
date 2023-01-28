bf7d774b8eaf2f00fb7d00673e6434972b8b7a4e
DJ-Dissociation <nicholasmkuntz@gmail.com>
2023-01-28 15:18:19 -0500

    achieve functionality on resizing

--- javascript.js
+++ javascript.js
@@ -1,29 +1,48 @@
 // Variable definitions
 const container = document.querySelector('.container');     // select container to hold all the squares
-// let dimension = 960;
+const containerWidth = 690; //px
+// let dimension = 320;
 // let nSquares = 16;
 
-// Create and add all squares in a grid
-for (i = 0; i < 16; i++){
-    const newRow = document.createElement('div');
-    newRow.classList.add('row');
+// Initialize first grid of 16 squares
+makeGrid(16);
+let squares = document.querySelectorAll('.colorSquare');
+activateSquares();
 
-    for (j = 0; j < 16; j++){
-        const newSquare = document.createElement('div');
-        newSquare.classList.add('colorSquare');
-        newRow.appendChild(newSquare);
+// Create and add all squares in a grid, given a # of squares as a dimension
+function makeGrid(dimension){
+    container.textContent = "";
+    let squareWidth = (containerWidth/dimension)+"px";
+    console.log(squareWidth);
+    console.log(typeof squareWidth);
+    for (i = 0; i < dimension; i++){    // for each row
+        const newRow = document.createElement('div');
+        newRow.classList.add('row');
+    
+        for (j = 0; j < dimension; j++){    // for each element per row
+            const newSquare = document.createElement('div');
+            newSquare.classList.add('colorSquare');
+            newSquare.style.width = squareWidth;
+            newSquare.style.height = squareWidth;
+            newRow.appendChild(newSquare);
+        }
+        container.appendChild(newRow);   
     }
-    container.appendChild(newRow);   
+    console.log("testing: "+container.style.height);
 }
 
-// Add ~hover~ eventListener to all squares, using 'mouseenter' event
-const squares = document.querySelectorAll('.colorSquare');
-squares.forEach((square) => {
-    square.addEventListener('mouseenter', () =>{
-        console.log("hovering");
-        square.classList.add('activeSquare');
+// Whenever run, this sweeps the document to reselect all squares and
+// update the nodelist
+function activateSquares() {
+    squares = document.querySelectorAll('.colorSquare');
+    squares.forEach((square) => {
+        square.addEventListener('mouseenter', () =>{
+            console.log("hovering");
+            square.classList.add('activeSquare');
+        });
     });
-});
+    return squares;
+}
 
 // Add a reset button that clears all squares when pressed
 const resetButton = document.querySelector('.resetButton');
@@ -33,8 +52,14 @@ resetButton.addEventListener('click', () => {
     })
 });
 
-// Add a resize button that prompts user for a dimension, then rebuilds grid (when pressed)
-const resizeButton = document.querySelector('resizeButton');
-resizeButton.addEventListener('click', () => {
-    //prompt resize, TODO implement
-});
+// A function to run when the Resize button is pressed; takes user input then
+// generates a new grid of appropriate size
+function resizeGrid(){
+    let dimension = parseInt(prompt("What size do you want?"));
+    if (dimension > 100) {
+        alert("Too big, sorry..");
+        return;
+    }
+    makeGrid(dimension);
+    squares = activateSquares();
+}
